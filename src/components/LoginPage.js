83748d337811a49f3e3c11709291e098dd3edc58
2200039139 <153254720+2200039139@users.noreply.github.com>
2025-06-04 12:41:49 +0530

    Update LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { FiArrowLeft,  FiCheck, FiInfo } from 'react-icons/fi';
+import './lg.css';
 
 const LoginPage = () => {
   const navigate = useNavigate();
